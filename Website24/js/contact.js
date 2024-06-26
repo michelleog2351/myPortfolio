20a946184fe95cde14fb542ca6d2e2d9c053e5db
michelleog2351 <132501606+michelleog2351@users.noreply.github.com>
2024-06-26 23:34:38 +0100

    Add contact page to website

---
+++ Website24/js/contact.js
@@ -0,0 +1,20 @@
+// Wait for the document to be fully loaded before executing any code
+$(document).ready(function () {
+  nav(); // Call the nav function to initialize navigation
+
+  $("#contactForm").submit(function (event) {
+    event.preventDefault(); // Prevent the form from submitting the traditional way
+
+    var fName = $("#firstName").val();
+    var lName = $("#lastName").val();
+    var email = $("#email").val();
+    // var password = $("#password").val();
+
+    if (fName && lName && email) {
+      alert("First Name: " + fName + "Last Name: " + lName + "Email: " + email);
+      $("#contactModal").modal("hide"); // Hide the modal after showing the alert
+    } else {
+      alert("Please fill in all fields.");
+    }
+  });
+});
