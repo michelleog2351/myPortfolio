58f383cd71e7c2328cf2ae70950d0aff68e44c95
michelleog2351 <132501606+michelleog2351@users.noreply.github.com>
2024-06-27 21:50:56 +0100

    Add carousel to music page. Allign navBar and text items to central position

--- Website24/js/nav.js
+++ Website24/js/nav.js
@@ -1,31 +1,30 @@
 /* Navigation function to dynamically generate navigation menu items */
 function nav() {
   $("nav").html(
-    `<ul class="navbar-nav">
+    `<div class="collapse navbar-collapse justify-content-center">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link text-dark" href="index.html">Home</a>
+        </li> 
 
-            <li class="nav-item">
-                <a class="nav-link text-dark" href="index.html">Home</a>
-            </li> 
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" 
+               href="about_me.html" 
+               role="button" 
+               data-bs-toggle="dropdown" 
+               aria-expanded="false">About Me</a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="art.html">Art</a></li>
+                <li><a class="dropdown-item" href="dance_music.html">Dance & Music</a></li>
+                <li><a class="dropdown-item" href="sports.html">Sport</a></li>
+            </ul>
+        </li> 
 
-            <li class="nav-item dropdown">
-            <li class="nav-item">
-                <a class="nav-link dropdown-toggle" 
-                href="hobbies.html 
-                role="button" 
-                data-bs-toggle="dropdown" 
-                aria-expanded="false">My Hobbies</a>
-
-                <ul class="dropdown-menu" aria-labelled="navbarDropdown">
-                  <li><a class="dropdown-item" href="art.html">Art</a></li>
-                  <li><a class="dropdown-item" href="music.html">Music</a></li>
-                  <li><a class="dropdown-item" href="sports.html">Sport</a></li>
-                </ul>
-            </li> 
-
-            <li class="nav-item">
-                <a class="nav-link text-dark" href="contact.html">Contact</a>
-            </li> 
-        </ul>`
+        <li class="nav-item">
+            <a class="nav-link text-dark" href="contact.html">Contact</a>
+        </li> 
+    </ul>
+    </div>`
   );
 
   // If user is logged in...
