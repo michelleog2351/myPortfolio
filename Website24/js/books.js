7fdc58ca9ace13355de424849f4c50ac551f7a4e
michelleog2351 <132501606+michelleog2351@users.noreply.github.com>
2024-07-08 18:18:49 +0100

    populate books dropdown with books names success

--- Website24/js/books.js
+++ Website24/js/books.js
@@ -1,6 +1,4 @@
 // Create an empty array to store data received from the database
-var data = [];
-var book1 = 1;
 
 // Wait for the document to be fully loaded before executing any code
 $(document).ready(function () {
@@ -9,11 +7,31 @@ $(document).ready(function () {
 });
 
 function getBooksDD() {
-  $.getJSON(`https://localhost:8000/json/books.json`, function (data) {
-    $.each(data.books, function (index, value) {
-      $("#books").append(
-        `<option value='${value.title}'>${value.title}</option>`
-      );
-    });
-  });
+
+  // AJAX request to fetch data from the server
+		$.ajax({
+			url: `http://localhost/myPortfolio/Website24/json/books.json`, // URL to fetch data from
+			cache: false, // NB!! Disable caching
+			type: "GET", // HTTP GET method
+			dataType: "json", // Expected data type
+			success: successFunc, // Callback function to handle successful response
+			error: errorFunc // Callback function to handle error
+		});
+
+    function successFunc(booksData)
+		{
+      $.each(booksData, function (index, value) {
+        $("#books").append(
+          `<option value='${value.title}'>${value.title}</option>`
+        );
+      });
+
+    }
+
+  // Callback function for AJAX error
+		function errorFunc(xhr,status,strError) 
+		{
+			$("#myDiv").text("There was an error!");
+			console.log("Error: "+ strError);
+		}
 }
