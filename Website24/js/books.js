9d2ea51293282570c8011d6e9d102bd8d0dbfdd5
michelleog2351 <132501606+michelleog2351@users.noreply.github.com>
2024-07-08 20:16:29 +0100

    Add table to display book information

--- Website24/js/books.js
+++ Website24/js/books.js
@@ -22,8 +22,50 @@ function getBooksDD() {
 		{
       $.each(booksData, function (index, value) {
         $("#books").append(
-          `<option value='${value.title}'>${value.title}</option>`
+          `<option value='${value.author}'>${value.author}</option>`
         );
+      });
+
+      // https://www.bookstation.ie/product/georges-marvellous-medicine-2/
+      // <a href="https://www.bookstation.ie/product/${value.title}-2">Info...</a>
+
+
+      $("#books").change(function() 
+    	{
+        // call the empty() function to clear the table to prevent data being overwritten
+        $("#tbody").empty();
+
+        var thisTable = "";
+
+        // Populate the table based on the selected team
+        $.each(booksData, function(index, value)
+        {
+          // if statement firstly to check which team is selected (its value) on the dropdown to print the corresponding table using the teamID
+          if($("#books").val() == value.author)
+          {
+            // Construct table rows for the selected county's players
+            thisTable += `<tr>`;
+            thisTable += `<td><img src="bookImages/${value.title}.jpg"></td>`;
+            thisTable += `<td>${value.title}, <br><b>${value.author}</b></td>`;
+            thisTable += `<td></td>`;
+            thisTable += `</tr>`;
+          // console.log(value);
+          }
+
+          // else if statement if the first option in the dropdown box is selected
+          // else if($("#counties").val() == "All"){
+          //   // Construct table rows for the selected team's players
+          //   thisTable += `<tr>`;
+          //   thisTable += `<td><img src="logos/${value.countyName}.png">${value.countyName}</td>`;
+          //   thisTable += `<td>${value.playerName}(#${value.squadNumber})</td>`;
+          //   thisTable += `<td>${value.age}</td>`;
+          //   thisTable += `<td>${value.matches}</td>`;
+          //   thisTable += `</tr>`;
+          //   //console.log(value);
+          // }
+        })  
+        $("#tbody").html(thisTable);
+
       });
 
     }
