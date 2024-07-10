6258573392d5494fa52a8f9af43fa7a8e225e4a9
michelleog2351 <132501606+michelleog2351@users.noreply.github.com>
2024-07-10 01:06:00 +0100

    add custom dialog when book image is clicked

--- Website24/js/books.js
+++ Website24/js/books.js
@@ -33,14 +33,33 @@ function getBooksDD() {
            {
             if ($("#books").val() == value.author || $("#books").val() == "All") {
               thisTable += `<tr>`;
-              thisTable += `<td><img src="bookImages/${value.title}.jpg"></td>`;
+              thisTable += `<td><img src="bookImages/${value.title}.jpg" class="book-image" data-title="${value.title}" data-author="${value.author}" data-price="${value.price.toFixed(2)}" data-description="${value.description}" data-image="bookImages/${value.title}.jpg" title="Click to view details"></td>`;
               thisTable += `<td>${value.title} <br><b>${value.author}</b></td>`;
-              thisTable += `<td>€${value.price}</td>`;
+              thisTable += `<td>€${value.price.toFixed(2)}</td>`;
               thisTable += `</tr>`;
             }
            });
         
         $("#tbody").html(thisTable);
+
+      // Attach click event to images
+      $(".book-image").click(function() {
+        var title = $(this).data("title");
+        var author = $(this).data("author");
+        var price = $(this).data("price");
+        var description = $(this).data("description");
+        var image = $(this).data("image");
+
+        // Update modal content
+        $("#modalTitle").text(title);
+        $("#modalAuthor").html(`<a href="https://www.google.com/search?q=${author}" target="_blank">${author}></a>`);
+        $("#modalPrice").text("€" + price);
+        $("#modalDesc").text(description);
+        $("#modalImage").attr("src", image); 
+
+      // Show the modal
+      $("#bookModal").modal('show');
+    });
       });
     }
 
@@ -52,7 +71,7 @@ function getInitialTB()
 			cache: false, // NB!! Disable caching
 			type: "GET", // HTTP GET method
 			dataType: "json", // Expected data type
-			 success: function(response) {
+			success: function(response) {
       // Populate booksData with fetched data
       booksData = response;
       
@@ -60,12 +79,31 @@ function getInitialTB()
       var thisTable = "";
       $.each(booksData, function(index, value) {
         thisTable += `<tr>`;
-        thisTable += `<td><img src="bookImages/${value.title}.jpg"></td>`;
-        thisTable += `<td>${value.title} <br><b>${value.author}</b></td>`;
-        thisTable += `<td>€${value.price}</td>`;
-        thisTable += `</tr>`;
+              thisTable += `<td><img src="bookImages/${value.title}.jpg" class="book-image" data-title="${value.title}" data-author="${value.author}" data-price="${value.price.toFixed(2)}" data-description="${value.description}" data-image="bookImages/${value.title}.jpg" title="Click to view details"></td>`;
+              thisTable += `<td>${value.title} <br><b>${value.author}</b></td>`;
+              thisTable += `<td>€${value.price.toFixed(2)}</td>`; 
+              thisTable += `</tr>`;
       });
       $("#tbody").html(thisTable); // Update the table body
+
+       // Attach click event to images
+      $(".book-image").click(function() {
+        var title = $(this).data("title");
+        var author = $(this).data("author");
+        var price = $(this).data("price");
+        var description = $(this).data("description");
+        var image = $(this).data("image");
+
+        // Update modal content
+        $("#modalTitle").text(title);
+        $("#modalAuthor").html(`<a href="https://www.google.com/search?q=${author}" target="_blank">${author}></a>`);
+        $("#modalPrice").text("€" + price);
+        $("#modalDesc").text(description);
+        $("#modalImage").attr("src", image); 
+
+        // Show the modal
+        $("#bookModal").modal('show');
+      });
     },
     error: function(xhr, status, strError) {
       $("#myDiv").text("There was an error!");
