b5da9fc2a15daf580419850845c703750d62ea77
michelleog2351 <132501606+michelleog2351@users.noreply.github.com>
2024-07-29 02:29:57 +0100

    BugFix to populate periods DD with periods, filtering not working yet

--- Website24/js/art.js
+++ Website24/js/art.js
@@ -1,144 +1,77 @@
 // Create an empty array to store data received from the database
-var artData = [];
+//var artData = [];
 // Wait for the document to be fully loaded before executing any code
 $(document).ready(function () {
   nav(); // Call the nav function to initialize navigation
   getInitialTB();
   //getBooksDD();
+
+  // Event listener for dropdown change
+  $("#periods").change(function () {
+    getInitialTB();
+  });
 });
 
-function getBooksDD() {
-  var artistsSet = new Set(); // Using Set to track unique authors
+function getBooksDD(artData) {
+  var periodsSet = new Set();
+  periodsSet.add("All");
 
-  // Collect unique authors
-  $.each(artData, function (index, value) {
-    artistsSet.add(value.author);
+  artData.forEach(painting => {
+    periodsSet.add(painting.period);
   });
 
-  // Convert the Set to an Array and sort it
-  var artistsArray = Array.from(artistsSet).sort();
-
-  // Populate the dropdown with sorted authors
-  $.each(artistsArray, function (index, author) {
-    $("#books").append(
-      `<option value='${artist}'>${artist}</option>`
-    );
-  });
-
-      // https://www.bookstation.ie/product/georges-marvellous-medicine-2/
-      // <a href="https://www.bookstation.ie/product/${value.title}-2">Info...</a>
-
-
-      $("#books").change(function() 
-    	{
-        // call the empty() function to clear the table to prevent data being overwritten
-        $("#tbody").empty();
-
-        var thisTable = "";
-
-        // // Sort artData by title alphabetically
-        // Sort artData by title alphabetically using an if statement approach
-        artData.sort(function(a, b) {
-          if (a.title < b.title) {
-            return -1;
-          }
-          if (a.title > b.title) {
-            return 1;
-          }
-          return 0;
+  var options = "";
+        periodsSet.forEach(period => {
+            options += `<option value="${period}">${period}</option>`;
         });
-        // artData.sort((a, b) => a.title.localeCompare(b.title));
-       
-           $.each(artData, function(index, value) 
-           {
-            if ($("#books").val() == value.period || $("#books").val() == "All") {
-              thisTable += `<tr>`;
-              thisTable += `<td><img src="pictures/art/${value.title}.jpg" class="book-image" data-title="${value.title}" data-author="${value.author}" data-price="${value.price.toFixed(2)}" data-description="${value.description}" data-image="bookImages/${value.title}.jpg" title="Click here to learn more about ${value.title}"></td>`;
-              thisTable += `<td>${value.title} <br><b>${value.author}</b></td>`;
-              thisTable += `<td>€${value.price.toFixed(2)}</td>`;
-              thisTable += `</tr>`;
-            }
-           });
-        
-        $("#tbody").html(thisTable);
 
-      // Attach click event to images
-      $(".book-image").click(function() {
-        var title = $(this).data("title");
-        var author = $(this).data("author");
-        var price = $(this).data("price");
-        var description = $(this).data("description");
-        var image = $(this).data("image");
-
-        // Update modal content
-        $("#modalTitle").text(title);
-        $("#modalAuthor").html(`<a href="https://www.google.com/search?q=${author}" target="_blank">${author}></a>`);
-        $("#modalDesc").text(description);
-        $("#modalImage").attr("src", image); 
-
-      // Show the modal
-      $("#bookModal").modal('show');
-    });
-  });
-}
+        $("#periods").html(options);
+        console.log("Dropdown Options:", options); // Debugging line
+    }
 
 function getInitialTB()
 	{
 		// AJAX request to fetch data from the server
 		$.ajax({
-			url: `http://localhost/myPortfolio/Website24/json/art/art.j`, // URL to fetch data from
+			url: `http://localhost/myPortfolio/Website24/json/art.json`, // URL to fetch data from
 			cache: false, // NB!! Disable caching
 			type: "GET", // HTTP GET method
 			dataType: "json", // Expected data type
-			success: function(response) {
-      // Populate artData with fetched data
-      artData = response;
-      
-      // Populate initial table (#tbody) with fetched data
-      var thisTable = "";
+			 success: function(response) {
+        console.log("Fetched Data:", response); // Debugging line
+        
+                if (response && response.paintings) {
+                    // Flatten the array of paintings grouped by artist
+                    const artData = response.paintings.flatMap(artistGroup => 
+                        artistGroup.paintings.map(painting => ({
+                            ...painting,
+                            artist: artistGroup.artist
+                        }))
+                    );
 
-      artData.sort(function(a, b) {
-          if (a.title < b.title) {
-            return -1;
-          }
-          if (a.title > b.title) {
-            return 1;
-          }
-          return 0;
-        });
+                    // Sort by title
+                    artData.sort((a, b) => a.title.localeCompare(b.title));
 
-      $.each(artData, function(index, value) {
-        thisTable += `<tr>`;
-              thisTable += `<td><img src="bookImages/${value.title}.jpg" class="book-image" data-title="${value.title}" data-author="${value.author}" data-price="${value.price.toFixed(2)}" data-description="${value.description}" data-image="bookImages/${value.title}.jpg" title="Click here to learn more about ${value.title}"></td>`;
-              thisTable += `<td>${value.title} <br><b>${value.author}</b></td>`;
-              thisTable += `<td>€${value.price.toFixed(2)}</td>`; 
-              thisTable += `</tr>`;
-      });
+                    // Populate the initial table (#tbody) with fetched data
+                    var thisTable = "";
+
+        $.each(artData, function(index, value) {
+                        if ($("#periods").val() == value.period || $("#periods").val() == "All") {
+                            thisTable += `<tr>`;
+                            thisTable += `<td><img src="pictures/art/${value.title}.jpg" alt="${value.title}"></td>`;
+                            thisTable += `</tr>`;
+            }
+           });
+
       $("#tbody").html(thisTable); // Update the table body
 
-       // Attach click event to images
-      $(".book-image").click(function() {
-        var title = $(this).data("title");
-        var author = $(this).data("author");
-        var price = $(this).data("price");
-        var description = $(this).data("description");
-        var image = $(this).data("image");
-
-        // Update modal content
-        $("#modalTitle").text(title);
-        $("#modalAuthor").html(`<a href="https://www.google.com/search?q=${author}" target="_blank">${author}></a>`);
-        $("#modalPrice").text("€" + price);
-        $("#modalDesc").text(description);
-        $("#modalImage").attr("src", image); 
-
-        // Show the modal
-        $("#bookModal").modal('show');
-      });
-
       // Populate the dropdown menu with authors
-      getBooksDD();
-    },
-    error: function(xhr, status, strError) {
+      getBooksDD(artData);
+     } else {
+                    console.error("No paintings data found in response.");
+                }
+            },
+      error: function(xhr, status, strError) {
       $("#myDiv").text("There was an error!");
       console.log("Error: " + strError);
     }
